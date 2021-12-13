804dd363a19ec4c59774159251aad33eface0024
昔音幻离 <Kittengarten@vip.qq.com>
2021-12-13 16:15:06 +0800

    Update start.sh

--- Minecraft_Java_Edition/start.sh
+++ Minecraft_Java_Edition/start.sh
@@ -1,1 +1,1 @@
-java -Xms1G -Xmx4G -XX:+UseZGC -Djava.awt.headless=true -Djava.util.concurrent.ForkJoinPool.common.parallelism=1 --enable-preview -jar paper-1.18.1-73.jar nogui
+java -Xms1280M -Xmx1280M -XX:+UseZGC -Djava.awt.headless=true -Djava.util.concurrent.ForkJoinPool.common.parallelism=1 --enable-preview -jar paper-1.18.1-73.jar nogui
