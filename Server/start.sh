39d7b68638eb2806fc95a5d7925312c55c0d449a
Kittengarten <kittengarten@vip.qq.com>
2023-01-29 14:11:43 +0800

    服务端更新 尝试修复基岩版下界问题 排除 KittenExpExPaper 的滥用限制 一些注释调整

--- Server/start.sh
+++ Server/start.sh
@@ -1,1 +1,1 @@
-java -Xms3G -Xmx3G -XX:+UseZGC -Djava.awt.headless=true --add-modules=jdk.incubator.vector -jar purpur-1.19.3-1895.jar nogui
+java -Xms3G -Xmx3G -XX:+UseZGC -Djava.awt.headless=true --add-modules=jdk.incubator.vector -jar purpur-1.19.3-1906.jar nogui
